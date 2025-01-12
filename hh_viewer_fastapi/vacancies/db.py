a66ee4ec0dcc9a441e43602dfb7ea8fb13353641
al-okdev <alokdev@mail.ru>
2025-01-12 14:25:46 +0300

    add aerich migrations hh_viewer_fastapi

--- hh_viewer_fastapi/vacancies/db.py
+++ hh_viewer_fastapi/vacancies/db.py
@@ -1,9 +1,10 @@
 from tortoise import Tortoise
+import config
 
 async def init():
-    await Tortoise.init(
-        db_url='sqlite://db.sqlite3',
-        modules={'models': ['vacancies.models']}
+    await Tortoise.init(config.TORTOISE_ORM
+        #db_url='sqlite://db.sqlite3',
+        #modules={'models': ['vacancies.models']}
     )
     # Generate the schema
-    await Tortoise.generate_schemas()
+    #await Tortoise.generate_schemas()
